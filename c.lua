e4bc46814a0f1267917ab2cfd92cd59e549fe93f
Kamil <40839443+kamil991@users.noreply.github.com>
2021-01-10 00:24:19 +0100

    v1.2
    the variables name update

--- c.lua
+++ c.lua
@@ -3,31 +3,32 @@
 @KONTAKT mój: discord / kamil#2087.
 @DO: GTAO.pl.
 @DATA: 2020-04-24.
+@RELEASE: 2021-01-10.
 @OPIS Szczegółowy: Panel HUD.
 ]]
 
 -- skalowanie odśrodkowe i czcionka
-local rozdzielczosc = {guiGetScreenSize()};
-local skalowanie = 1920/rozdzielczosc[1];
-local czcionka_pieniadze = dxCreateFont("misc/pieniadze.ttf", 18/skalowanie);
-local czcionka_regular = dxCreateFont("misc/Roboto-Regular.ttf", 18/skalowanie);
+local screen_res = {guiGetScreenSize()};
+local scale_res = 1920/screen_res[1];
+local font_cash = dxCreateFont("misc/pieniadze.ttf", 18/scale_res);
+local font_regular = dxCreateFont("misc/Roboto-Regular.ttf", 18/scale_res);
 hud = {}
 
 hud.render = function()
 
-	local pieniadze = getPlayerMoney(localPlayer);
-	local pieniadzestring = string.format("%.02f",pieniadze);
-	local czas = getRealTime();
-	local godziny = czas.hour;
-	local minuty = czas.minute;
-    local czasf = string.format("%02d:%02d", godziny, minuty);
-	local nazwa = getPlayerName(localPlayer);
+	cash = getPlayerMoney(localPlayer);
+	cashstring = string.format("%.02f",cash);
+	gettime = getRealTime();
+	hours = gettime.hour;
+	minutes = gettime.minute;
+        timef = string.format("%02d:%02d", hours, minutes);
+	plrname = getPlayerName(localPlayer);
 
-	dxDrawImage(rozdzielczosc[1] - 505/skalowanie, rozdzielczosc[2] - 1025/skalowanie, 490/skalowanie, 100/skalowanie, 'img/background.png', angle, 0, -120 )
-	dxDrawImage(rozdzielczosc[1] - 497/skalowanie, rozdzielczosc[2] - 1015/skalowanie, 82/skalowanie, 82/skalowanie, 'img/avatar.png', angle, 0, -120 )
-	dxDrawText(nazwa.."",rozdzielczosc[1] - 520/skalowanie, rozdzielczosc[2] - 1975/skalowanie, 1920/skalowanie, 1080/skalowanie,tocolor(255,255,255),1, czcionka_regular,"center","center",false,false,false,true,false )
-	dxDrawText(pieniadzestring.."€",rozdzielczosc[1] - 520/skalowanie, rozdzielczosc[2] - 1915/skalowanie, 1920/skalowanie, 1080/skalowanie,tocolor(255,255,255),1, czcionka_pieniadze,"center","center",false,false,false,true,false )
-	dxDrawText(czasf,rozdzielczosc[1] - 170/skalowanie, rozdzielczosc[2] - 1945/skalowanie, 1920/skalowanie, 1080/skalowanie,tocolor(255,255,255),1.0, czcionka_regular,"center","center",false,false,false,true,false )
+	dxDrawImage(screen_res[1] - 505/scale_res, screen_res[2] - 1025/scale_res, 490/scale_res, 100/scale_res, 'img/background.png', angle, 0, -120 )
+	dxDrawImage(screen_res[1] - 497/scale_res, screen_res[2] - 1015/scale_res, 82/scale_res, 82/scale_res, 'img/avatar.png', angle, 0, -120 )
+	dxDrawText(plrname.."",screen_res[1] - 520/scale_res, screen_res[2] - 1975/scale_res, 1920/scale_res, 1080/scale_res,tocolor(255,255,255),1, font_regular,"center","center",false,false,false,true,false )
+	dxDrawText(cashstring.."€",screen_res[1] - 520/scale_res, screen_res[2] - 1915/scale_res, 1920/scale_res, 1080/scale_res,tocolor(255,255,255),1, font_cash,"center","center",false,false,false,true,false )
+	dxDrawText(timef,screen_res[1] - 170/scale_res, screen_res[2] - 1945/scale_res, 1920/scale_res, 1080/scale_res,tocolor(255,255,255),1.0, font_regular,"center","center",false,false,false,true,false )
 
 end
 
